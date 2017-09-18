72522b989697bd4fdbd94c8b1fcd81b78940c33e
Mario Suharja <mariostefanos@gmail.com>
2017-09-19 00:36:03 +0700

    feat: Set Basic UI
    - Set basic container UI using Semantic-UI

--- src/app/containers/PostListContainer/PostListContainer.js
+++ src/app/containers/PostListContainer/PostListContainer.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { PostList } from 'app/components';
 
 class PostListContainer extends Component {
+  componentDidMount () {
+    console.log('Props from Post List Container: ', this.props)
+  }
+
   render () {
     return (
       <PostList />
--- src/app/containers/index.js
+++ src/app/containers/index.js
@@ -1,5 +1,7 @@
+import MainContainerF from './MainContainer/MainContainer'
 import PostListContainerF from './PostListContainer/PostListContainer'
 import PostDetailContainerF from './PostDetailContainer/PostDetailContainer'
 
+export const MainContainer = MainContainerF
 export const PostListContainer = PostListContainerF
 export const PostDetailContainer = PostDetailContainerF
